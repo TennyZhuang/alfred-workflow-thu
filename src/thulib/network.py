3bc99805c8b32359bf56ae2274f7af7ad74d03d0
TennyZhuang <zty0826@gmail.com>
2016-05-10 23:59:12 +0800

    login

--- src/thulib/network.py
+++ src/thulib/network.py
@@ -1,5 +1,14 @@
+from workflow.web import request
+
 def login(username, md5pwd):
-    pass
+    data = {
+        'action': 'login',
+        'username': username,
+        'password': u'{MD5_HEX}' + md5pwd,
+        'ac_id': 1
+    }
+
+    res = request('POST', 'http://net.tsinghua.edu.cn/do_login.php', data=data)
 
 def logout():
     pass
