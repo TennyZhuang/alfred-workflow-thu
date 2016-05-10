e951b1b385705b0497cc7da44a8f085c89e1526e
TennyZhuang <zty0826@gmail.com>
2016-05-10 23:44:35 +0800

    entry script

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import sys
+from hashlib import md5
+from binascii import hexlify
+from workflow import Workflow
+from workflow.workflow import Settings
+from thulib.network import login
+
+log = None
+
+def main(wf):
+    args = wf.args
+    log.debug(args)
+
+    if args[0] == u'--login':
+        username = wf.get_password(u'username')
+        password = wf.get_password(u'password')
+        log.debug(u'username: %s, password: %s' % (username, password))
+        login(username, password)
+    elif args[0] == u'--account':
+        username = wf.save_password('username', args[1])
+        password = wf.save_password('password',
+            hexlify(md5(args[2].encode('latin-1')).digest()))
+
+if __name__ == '__main__':
+    wf = Workflow()
+    log = wf.logger
+    sys.exit(wf.run(main))
