7d0faa8ba240d59c79fefd5fa89c4be913b8a2b1
TennyZhuang <zty0826@gmail.com>
2016-05-11 08:56:11 +0800

    notify result

--- src/thulib/network.py
+++ src/thulib/network.py
@@ -1,4 +1,8 @@
 from workflow.web import request
+from workflow.notify import notify
+from urllib2 import URLError
+
+LOGIN_URL = 'http://net.tsinghua.edu.cn/do_login.php'
 
 def login(username, md5pwd):
     data = {
@@ -8,7 +12,9 @@ def login(username, md5pwd):
         'ac_id': 1
     }
 
-    res = request('POST', 'http://net.tsinghua.edu.cn/do_login.php', data=data)
+    try:
+        res = request('POST', LOGIN_URL, data=data)
+    except URLError as e:
+        notify(title=str(e.errno), text=str(e.reason))
 
-def logout():
-    pass
+    notify(title=str(res.status_code), text=res.text)
