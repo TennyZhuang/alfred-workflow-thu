a945ac634689e0e74b17baffaae11ea17ae48325
庄天翼 <zty0826@gmail.com>
2017-12-10 10:35:26 +0800

    Using https in academic login API

--- src/thulib/academic.py
+++ src/thulib/academic.py
@@ -3,7 +3,7 @@ from thulib import proxy_to_tsinghua
 @proxy_to_tsinghua
 def academic(username, password):
     return {
-        'action': 'http://academic.tsinghua.edu.cn:80/Login',
+        'action': 'https://academic.tsinghua.edu.cn:80/Login',
         'inputs': [
             { 'name': 'userName', 'value': username },
             { 'name': 'password', 'value': password }
