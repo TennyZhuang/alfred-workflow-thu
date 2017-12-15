79e35747af2fe4660aba0aeec02daf292bd3372c
庄天翼 <zty0826@gmail.com>
2017-12-15 22:44:37 +0800

    Fix: remove port in url

--- src/thulib/academic.py
+++ src/thulib/academic.py
@@ -3,7 +3,7 @@ from thulib import proxy_to_tsinghua
 @proxy_to_tsinghua
 def academic(username, password):
     return {
-        'action': 'https://academic.tsinghua.edu.cn:80/Login',
+        'action': 'https://academic.tsinghua.edu.cn/Login',
         'inputs': [
             { 'name': 'userName', 'value': username },
             { 'name': 'password', 'value': password }
